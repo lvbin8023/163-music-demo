f957cbc8e20a3c191f54174017a822a6d367fde6
lvbin <472245767@qq.com>
2019-01-30 17:58:59 +0800

    add pages

--- config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  'AccessKey': 'y5mrF5q9lZNXly_UEqB48OIL7oKmaJmctghOuTe7',
-  'SecretKey': 'RqpXMNOBWeud-RvNsdrM1KucRPxj12B0KMCHfKh5',
-  'Bucket': '163-music-demo',
-  'Port': 19110,
-  'UptokenUrl': 'uptoken', // demo 启动后会在本地 /uptoken 上提供获取 uptoken 的接口，所以这里可以填 'uptoken'
-  'Domain': 'http://pm4x90zhk.bkt.clouddn.com/' // Bucket 的外链默认域名，在 Bucket 的内容管理里查看，如：'http://xxx.bkt.clouddn.com/'
-};
